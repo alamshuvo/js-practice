81f55b7750382a577640b43b4e97c2164ed1a4e7
iftakhar <alamshuvozeny019@gmail.com>
2023-11-15 09:26:35 +0600

    arrow-function

---
+++ arrow-function.js
@@ -0,0 +1,39 @@
+// let myFunction = (a, b) => a * b;
+
+// myFunction(7,5);
+// const another =(a,b)=> {
+// let sum = a+b ;
+// return sum;
+
+// }
+// another(5,5);
+
+//  const myFunction=(a,b,c)=>{
+//     let sum = a+b+c;
+//     return sum;
+//  }
+// console.log(myFunction(5,6,3)) ;
+
+
+
+
+// const myFunction=(a,b)=> a+b+200
+// console.log(myFunction(200,300));
+
+
+
+
+//  another=(a, b) => {
+//     const chuck = 42;
+//     return a + b + chuck;
+//   };
+//   console.log (another(20,30));
+
+
+
+// syntax of arrow function 
+// const arrowSyntax=(param)=>{
+//     // write code 
+
+// }
+// console.log(arrowSyntax());
